b0d239cb00952189bac341c8150ece62421515fc
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-06-29 09:30:51 -0700

    project setup

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD7dqYjnHYU57wofdppv7bCDLlrKuK3jyk",
+  authDomain: "linkbush-d16ce.firebaseapp.com",
+  projectId: "linkbush-d16ce",
+  storageBucket: "linkbush-d16ce.appspot.com",
+  messagingSenderId: "983928522861",
+  appId: "1:983928522861:web:b35aea0f5db153c23a9764",
+  measurementId: "G-VYL31YV32R",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const analytics = getAnalytics(app);
